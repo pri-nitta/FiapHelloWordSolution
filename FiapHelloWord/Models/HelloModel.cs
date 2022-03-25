0fe65dc8c783c1c0222a1945af58939dc9087395
Priscila Nitta <priscila.nitta@gmail.com>
2022-03-25 15:13:51 -0300

    creating new model

---
+++ FiapHelloWord/Models/HelloModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiapHelloWord.Models
+{
+    public class HelloModel
+    {
+        public String Mensagem = "Ola Model C#";
+    }
+}
--- FiapHelloWord/Program.cs
+++ FiapHelloWord/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using FiapHelloWord.Models;
+
 Console.WriteLine("Hello, World!");
+HelloModel model = new HelloModel();
+Console.WriteLine(model.Mensagem);
 Console.Read();
